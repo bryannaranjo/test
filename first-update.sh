2205cb7278589f96122f6802764ad15a95a5455e
Bryan Naranjo <bryan@naranjo.com>
2022-10-07 12:25:04 -0400

    fixed sudo

--- first-update.sh
+++ first-update.sh
@@ -3,6 +3,8 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install openssh-server sshpass -y
 
-mkdir /media/xdrive
+sudo mkdir /media/xdrive
 sudo mount -t nfs 172.16.0.76:/volume7/NFS-Xdrive /media/xdrive
 
+git config --global user.email "bryan@naranjo.com"
+git config --global user.name "Bryan Naranjo"
