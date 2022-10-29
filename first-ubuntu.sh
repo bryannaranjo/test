15124efedddfcb146119df7f8f6867b132ce6c38
Bryan Naranjo <bryan@naranjo.com>
2022-10-29 15:26:17 -0400

    added packages

--- first-ubuntu.sh
+++ first-ubuntu.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install openssh-server sshpass -y
+sudo apt install openssh-server sshpass ansible nfs-common -y
 
 sudo mkdir /media/xdrive
 sudo mount -t nfs 172.16.0.76:/volume7/NFS-Xdrive /media/xdrive
